d238f2d145254ee16c4a17ce0a851d536efd942e
Mia <tamiapaschall@gmail.com>
2020-01-19 02:30:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,29 +1,13 @@
 class Backer
-  attr_accessor :name, :artist 
-  @@all = []
+  attr_reader :name, :backed_projects
   
   def initialize(name)
     @name = name 
-    save
+    @backed_projects = []
   end
   
-  def save 
-    @@all << self 
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist 
-    new_song
-  end
-  
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+  def backed_projects=(project)
+    backed_projects << project
+    project.backers << self 
+    end
 end
