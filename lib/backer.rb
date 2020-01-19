dd1600f86a72830fab96a25616aeddbe0ff12962
Mia <tamiapaschall@gmail.com>
2020-01-19 02:36:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,8 @@ class Backer
     @backed_projects = []
   end
   
-  def back_projects(project)
-    back_projects << project
+  def back_project(project)
+    backed_projects << project
     project.backers << self 
     end
 end
